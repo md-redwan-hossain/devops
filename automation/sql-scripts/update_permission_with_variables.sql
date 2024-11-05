c9818f33da7a4c442c1d710bbfc9bc309202ae1e
md-redwan-hossain <redwan.ping@gmail.com>
2024-11-05 08:59:55 +0600

    added function perm

--- automation/sql-scripts/update_permission_with_variables.sql
+++ automation/sql-scripts/update_permission_with_variables.sql
@@ -19,14 +19,18 @@ GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA :schema_to_create TO :admin_user
 -- Set default privileges for the admin user
 ALTER DEFAULT PRIVILEGES IN SCHEMA :schema_to_create GRANT ALL PRIVILEGES ON TABLES TO :admin_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA :schema_to_create GRANT ALL PRIVILEGES ON SEQUENCES TO :admin_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA :schema_to_create GRANT ALL PRIVILEGES ON FUNCTIONS TO :admin_user;
 
 -- Grant limited privileges to the regular user
 GRANT CONNECT ON DATABASE :dbname TO :regular_user;
 GRANT USAGE ON SCHEMA :schema_to_create TO :regular_user;
 
--- Grant full privileges excluding DROP
+-- Grant full privileges excluding DROP on tables and sequences
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA :schema_to_create TO :regular_user;
 GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA :schema_to_create TO :regular_user;
+
+-- Grant EXECUTE privilege on all existing functions to the regular user
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA :schema_to_create TO :regular_user;
 
 -- Set default privileges for the regular user for new objects created by admin_user
 ALTER DEFAULT PRIVILEGES FOR ROLE :admin_user IN SCHEMA :schema_to_create
@@ -34,3 +38,6 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO :regular_user;
 
 ALTER DEFAULT PRIVILEGES FOR ROLE :admin_user IN SCHEMA :schema_to_create
 GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO :regular_user;
+
+ALTER DEFAULT PRIVILEGES FOR ROLE :admin_user IN SCHEMA :schema_to_create
+GRANT EXECUTE ON FUNCTIONS TO :regular_user;
