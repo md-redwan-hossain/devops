f625a216b01f5d4a29c6a8e4674ea25e285817cc
Md. Redwan Hossain <redwan.ping@gmail.com>
2025-11-25 12:54:56 +0600

    Update setup_db.sql

--- automation/sql-scripts/setup_db.sql
+++ automation/sql-scripts/setup_db.sql
@@ -1,3 +1,11 @@
+-- Control flags
+\set create_database TRUE
+\set create_admin_user TRUE
+\set create_schema FALSE
+\set create_regular_user FALSE
+\set create_readonly_user FALSE
+\set drop_existing FALSE
+
 -- Set variables for the database, schema, users, and passwords
 \set dbname 'test_db'
 \set schema_to_create 'test_schema'
@@ -8,36 +16,46 @@
 \set readonly_user 'test_db_readonly'
 \set readonly_user_password 'readonly_user_password'
 
-
-\if TRUE
-
-\if FALSE
+-- Drop existing resources if flag is set
+\if :drop_existing
 DROP DATABASE IF EXISTS :dbname;
 DROP USER IF EXISTS :admin_user;
 DROP USER IF EXISTS :regular_user;
 DROP USER IF EXISTS :readonly_user;
-
 \endif
 
--- Create the database
+-- Create the database if flag is set
+\if :create_database
 CREATE DATABASE :dbname;
-
--- Connect to the database
-\c :dbname
-
--- Create schema
-CREATE SCHEMA :schema_to_create;
-
--- Create users
-CREATE USER :admin_user WITH ENCRYPTED PASSWORD :'admin_password';
-CREATE USER :regular_user WITH ENCRYPTED PASSWORD :'regular_user_password';
-CREATE USER :readonly_user WITH ENCRYPTED PASSWORD :'readonly_user_password';
-
 \endif
 
-
 -- Connect to the database
 \c :dbname
+
+-- Create schema if flag is set
+\if :create_schema
+CREATE SCHEMA :schema_to_create;
+\endif
+
+-- Create admin user if flag is set
+\if :create_admin_user
+CREATE USER :admin_user WITH ENCRYPTED PASSWORD :'admin_password';
+\endif
+
+-- Create regular user if flag is set
+\if :create_regular_user
+CREATE USER :regular_user WITH ENCRYPTED PASSWORD :'regular_user_password';
+\endif
+
+-- Create readonly user if flag is set
+\if :create_readonly_user
+CREATE USER :readonly_user WITH ENCRYPTED PASSWORD :'readonly_user_password';
+\endif
+
+-- ============================================================================
+-- GRANT PRIVILEGES TO ADMIN USER
+-- ============================================================================
+\if :create_admin_user
 
 -- Grant full privileges to the admin user on test_schema
 GRANT ALL PRIVILEGES ON DATABASE :dbname TO :admin_user;
@@ -60,6 +78,13 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA :schema_to_create GRANT ALL PRIVILEGES ON FUN
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO :admin_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON SEQUENCES TO :admin_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON FUNCTIONS TO :admin_user;
+
+\endif
+
+-- ============================================================================
+-- GRANT PRIVILEGES TO REGULAR USER
+-- ============================================================================
+\if :create_regular_user
 
 -- Grant limited privileges to the regular user on test_schema
 GRANT CONNECT ON DATABASE :dbname TO :regular_user;
@@ -100,6 +125,12 @@ GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO :regular_user;
 ALTER DEFAULT PRIVILEGES FOR ROLE :admin_user IN SCHEMA public
 GRANT EXECUTE ON FUNCTIONS TO :regular_user;
 
+\endif
+
+-- ============================================================================
+-- GRANT PRIVILEGES TO READONLY USER
+-- ============================================================================
+\if :create_readonly_user
 
 -- Grant privileges to the read-only user on test_schema
 GRANT CONNECT ON DATABASE :dbname TO :readonly_user;
@@ -145,3 +176,5 @@ GRANT USAGE, SELECT ON SEQUENCES TO :readonly_user;
 
 ALTER DEFAULT PRIVILEGES FOR ROLE :admin_user IN SCHEMA public
 GRANT EXECUTE ON FUNCTIONS TO :readonly_user;
+
+\endif
