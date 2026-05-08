c135a1d33f5e581315c64201c6819e1c718606ef
md-redwan-hossain <redwan.ping@gmail.com>
2026-05-09 01:18:14 +0600

    Update Gitea run script to create data and database directories under 'container_data'.

--- docker/gitea/run_me.sh
+++ docker/gitea/run_me.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p gitea_data 
+mkdir -p container_data/gitea_data 
 chown 1000:1000 gitea_data
-mkdir gitea_db -p 
+mkdir container_data/gitea_db -p 
 chown 999:999 gitea_db
