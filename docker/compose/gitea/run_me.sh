b271f54355e62a96ec0f4d592b18b7ff71282226
md-redwan-hossain <redwan.ping@gmail.com>
2026-05-10 18:40:23 +0600

    Update Gitea run script to use 'sudo' for changing ownership of data directories, ensuring proper permissions are set for container data.

--- docker/compose/gitea/run_me.sh
+++ docker/compose/gitea/run_me.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p container_data/gitea_data 
-chown 1000:1000 container_data/gitea_data 
+mkdir -p container_data/gitea_data
+sudo chown 1000:1000 container_data/gitea_data
 mkdir -p container_data/gitea_db
-chown 999:999 container_data/gitea_db
+sudo chown 999:999 container_data/gitea_db
 mkdir -p container_data/gitea_runner_data
-chown 1000:1000 container_data/gitea_runner_data
+sudo chown 1000:1000 container_data/gitea_runner_data
