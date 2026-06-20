88b125b59147ad741b670407b7a1b80de9572304
md-redwan-hossain <redwan.ping@gmail.com>
2026-06-20 18:37:38 +0600

    clean up

--- docker/compose/rustfs/run_me.sh
+++ docker/compose/rustfs/run_me.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p rustfs_data
-mkdir -p rustfs_logs
+mkdir -p rustfs_data rustfs_logs
+chown -R 10001:10001 rustfs_data rustfs_logs
 
 docker network inspect rustfs_network >/dev/null 2>&1 || docker network create rustfs_network
