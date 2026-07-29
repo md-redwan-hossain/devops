ea6a678460335931b802a0e85997c864eea587c8
Md. Redwan Hossain <redwan.ping@gmail.com>
2026-07-29 21:22:17 +0600

    added gitea external db

---
+++ docker/compose/gitea-external-db/gitea/run_me.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir -p container_data/gitea_data
+sudo chown 1000:1000 container_data/gitea_data
+mkdir -p container_data/gitea_db
+sudo chown 999:999 container_data/gitea_db
+mkdir -p container_data/gitea_runner_data
+sudo chown 1000:1000 container_data/gitea_runner_data
+
+docker network inspect gitea_network >/dev/null 2>&1 || docker network create gitea_network
