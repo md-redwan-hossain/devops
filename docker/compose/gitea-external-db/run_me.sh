f366007142f51bac25aaa3eb6c0ad6e0ca4c39f1
Md. Redwan Hossain <redwan.ping@gmail.com>
2026-07-29 21:37:03 +0600

    dropped bloat

--- docker/compose/gitea-external-db/run_me.sh
+++ docker/compose/gitea-external-db/run_me.sh
@@ -2,8 +2,6 @@
 
 mkdir -p container_data/gitea_data
 sudo chown 1000:1000 container_data/gitea_data
-mkdir -p container_data/gitea_db
-sudo chown 999:999 container_data/gitea_db
 mkdir -p container_data/gitea_runner_data
 sudo chown 1000:1000 container_data/gitea_runner_data
 
